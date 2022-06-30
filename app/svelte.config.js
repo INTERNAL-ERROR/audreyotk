8d264dac09698d970ce6776ab94d413c476a0e4c
INTERNAL-ERROR <morrowoscar33@gmail.com>
2022-06-30 16:02:00 +0000

    hi

---
+++ app/svelte.config.js
@@ -0,0 +1,21 @@
+/*@type {import('@sveltejs/kit').Config}*/
+import adapter from '@sveltejs/adapter-netlify';
+
+export default {
+	kit: {
+		adapter: adapter({
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
+		}),
+       vite: {
+           server: {
+                host: '0.0.0.0',
+                hmr: {
+                  port: 443,
+                }
+            }
+        }
+	},
+
+};
